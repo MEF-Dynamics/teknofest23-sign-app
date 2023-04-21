69b01f2061dc2f7b7d015c9e9c1a82668bf00e04
Emir Çetin MEMİŞ <zeymenak@gmail.com>
2023-04-21 04:40:57 +0300

    Create hashes.py

---
+++ Constants/hashes.py
@@ -0,0 +1,17 @@
+ANIM_HASHES = {
+    "sen" : ["seni", "sana", "senin", "senle"],
+    "ne" : ["nedir", "ney"],
+    "adin" : ["ad", "adi", "adlari", "adlar"],
+    "gel" : ["gelir", "geldi", "geliyor"],
+    "seviyorum" : ["seviyor", "seviyorlar", "seviyoruz", "sev", "sevmek"],
+    "neden" : ["nicin"],
+}
+
+TURKISH_CHARS = {
+    "ğ" : "g",
+    "ü" : "u",
+    "ş" : "s",
+    "ö" : "o",
+    "ç" : "c",
+    "ı" : "i",
+}
